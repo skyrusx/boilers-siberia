3a4654e454d9b4bcf73a35d8cbf560b9a5265cc8
Ruslan Fedotov <progruson@gmail.com>
2024-02-07 18:31:27 +0700

    Fix application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,3 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
-// import "popper"
-import "bootstrap"
+//= require_tree ./application
+//= import "popper"
+//= import "bootstrap"
