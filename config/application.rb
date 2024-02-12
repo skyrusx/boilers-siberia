89895a880a0a2633fcf6cca711fdb1a99dc3b330
Ruslan Fedotov <progruson@gmail.com>
2024-02-12 20:16:30 +0700

    Add default locale

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module App
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.i18n.default_locale = :ru
   end
 end
