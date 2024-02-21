b7c9b9a14662768a6909dbc4b4c364f20abca017
Ruslan Fedotov <progruson@gmail.com>
2024-02-22 02:49:16 +0700

    Fix product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,10 @@ class ProductsController < AdminController
 
   def show
     @product = Product.find(params[:id])
-    @order_item = current_order.order_items.new
+    @order_item = OrderItem.where(product_id: @product.id).first
+    unless @order_item.present?
+      @order_item = current_order.order_items.new
+    end
   end
 
   def new
