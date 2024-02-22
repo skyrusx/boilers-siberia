30a13430f139ebb12f3edc3899cd11ccea1845f3
Ruslan Fedotov <progruson@gmail.com>
2024-02-22 19:58:46 +0700

    Add PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,21 @@
 class PagesController < ApplicationController
   def landing
+    @settings = Setting.all.first
+
+    @products_all = Product.all
+    @products = @products_all.where(product_type: "Товар")
+    @services = @products_all - @products
+
+    @groups = Group.all
+    @groups_left = @groups.first(2)
+    @groups_right = @groups - @groups_left
+
+    @activities = Activity.all
+    @reviews = Review.all
     @advantages = Advantage.all
+    @bonuses = Bonuse.all
+
     @contacts = Contact.order("region DESC")
-    @main_contact = @contacts.first
     @social_links = SocialLink.all
   end
 end
