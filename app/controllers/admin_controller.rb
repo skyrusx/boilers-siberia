54a962a04ba5eb51fdaf34703c1d7067f9b18909
Ruslan Fedotov <progruson@gmail.com>
2024-02-24 15:50:30 +0700

    Add info to admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,5 +3,15 @@ class AdminController < ApplicationController
   layout 'admin'
 
   def index
+    @products_all = Product.all
+    @products = @products_all.where(product_type: "Товар")
+    @services = @products_all - @products
+
+    @messages_all = Message.all
+    @messages = @messages_all.where(type_feedback: :appeal)
+    @orders = @messages_all - @messages
+
+    @new_messages = @messages.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    @new_orders = @messages.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
   end
 end
