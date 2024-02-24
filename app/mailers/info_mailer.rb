bcd80f9eb680897bd52ec5a61560edda806cf74d
Ruslan Fedotov <progruson@gmail.com>
2024-02-24 13:09:14 +0700

    Microfix

--- app/mailers/info_mailer.rb
+++ app/mailers/info_mailer.rb
@@ -1,6 +1,6 @@
 class InfoMailer < ApplicationMailer
 
-  def send_message()
+  def send_message
     @message = params[:message]
     @subject = "#{Message::TYPES[@message.type_feedback.to_sym]} c сайта Котлы Сибири"
     mail(to: User.first.email, from: ENV["EMAIL_ADDRESS"], subject: @subject)
