d25bff4fc9d9e0c9b9c231435ed7987a52512c51
Ruslan Fedotov <progruson@gmail.com>
2024-02-24 13:08:20 +0700

    Fix InfoMailer

--- app/mailers/info_mailer.rb
+++ app/mailers/info_mailer.rb
@@ -1,9 +1,8 @@
 class InfoMailer < ApplicationMailer
-  default from: "manager@boilers-siberia.ru"
 
   def send_message()
     @message = params[:message]
     @subject = "#{Message::TYPES[@message.type_feedback.to_sym]} c сайта Котлы Сибири"
-    mail(to: 'phedotov-rus@yandex.ru', from: "manager@boilers-siberia.ru", subject: @subject)
+    mail(to: User.first.email, from: ENV["EMAIL_ADDRESS"], subject: @subject)
   end
 end
