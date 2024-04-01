d0e42b7e743bde91c94538f44e8dfe8d054e73ab
Ruslan Fedotov <progruson@gmail.com>
2024-04-01 22:50:19 +0700

    Fix bonuses

--- app/models/bonuse.rb
+++ app/models/bonuse.rb
@@ -1,3 +1,6 @@
 class Bonuse < ApplicationRecord
-  has_one_attached :banner
+  has_one_attached :banner do |attachable|
+    attachable.variant :preview, resize_to_limit: [408, 200]
+    attachable.variant :admin, resize_to_limit: [153, 75]
+  end
 end
