38776058b06226685c90abd488858b6bb3abfd69
Ruslan Fedotov <progruson@gmail.com>
2024-04-01 22:57:45 +0700

    Fix reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
-  has_one_attached :avatar
+  has_one_attached :avatar do |attachable|
+    attachable.variant :preview, resize_to_limit: [112, 112]
+    attachable.variant :admin, resize_to_limit: [75, 75]
+  end
 end
