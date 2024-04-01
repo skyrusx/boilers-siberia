39b35e2226afb52836110378458907d1ae89f750
Ruslan Fedotov <progruson@gmail.com>
2024-04-01 22:34:34 +0700

    Fix has_one_attached

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,6 @@
 class Activity < ApplicationRecord
-  has_one_attached :icon
+  has_one_attached :icon do |attachable|
+    attachable.variant :preview, resize_to_limit: [115, 115]
+    attachable.variant :admin, resize_to_limit: [75, 75]
+  end
 end
